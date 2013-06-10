8684b53924367769450d574ba765cd03f0a003be
Johan Binard <johan.binard@gmail.com>
2013-06-10 09:41:14 +0200

    Meilleure séparation vues / controleurs

---
+++ vues/traitement_envoi.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Traitement</title>
+	</head>
+	
+	<body>
+		<?php if(isset($resultat_traitement_video)) echo '<p>' . $resultat_traitement_video . '</p>' ;?>
+		<?php if(isset($resultat_traitement_srt)) echo '<p>' . $resultat_traitement_srt . '</p>' ;?>
+	</body>
+</html>
+
